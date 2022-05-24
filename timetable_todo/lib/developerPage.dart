f91a9483f828d79ef0dc739699d1642b55b005ae
Daeun Kim <gammaeom1@naver.com>
2022-05-24 14:15:37 +0900

    Update: developerPage 코드작성,  image 파일 생성, 미모티콘 4개 추가

--- timetable_todo/lib/developerPage.dart
+++ timetable_todo/lib/developerPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class developerPage extends StatefulWidget{
   const developerPage({Key? key}) : super(key: key);
 
@@ -11,11 +12,212 @@ class developerPage extends StatefulWidget{
 class _developerPage extends State<developerPage>{
   Widget build(BuildContext context){
     return Scaffold(
+      /*appBar: AppBar(title: Text('개발자 김유정 고2',style: TextStyle(color: Colors.black),),
+        backgroundColor: Color(0xffF5F4F2),
+      ),*/
       body: Container(
-        child: Column(
-          children: [
-            Text('developerPage')
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                  width: 400.0,
+                  height: 149.0,
+                  decoration: BoxDecoration(
+                    color: Color(0xffECECEC),
+                  ),
+                  child: Row(
+                      children:<Widget>[
+                        Container(
+                            width:135,
+                            child: Image.asset('image/다은 미모티콘.png')
+                        ),
+                        Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text('김 다 은 Kim Da Eun' ,style: TextStyle(
+                                  shadows:[
+                                    Shadow(
+                                      color: Colors.grey.withOpacity(1),
+                                      blurRadius: 7,
+                                      offset: Offset(0, 3),
+                                    )
+                                  ],
+                                  fontFamily:'Quantico',fontWeight:FontWeight.bold,fontSize: 23.0)),
+                              Text('\n컴퓨터공학부 20191101',style: TextStyle(
+                                  shadows:[
+                                    Shadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      blurRadius: 7,
+                                      offset: Offset(0, 3),
+                                    )
+                                  ],
+                                  fontFamily:'Quantico',fontWeight:FontWeight.bold,color: Color(0xff5C5C5C),fontSize: 13.0)),
+                              Text('Email : 20191101@vision.hoseo.edu' ,style: TextStyle(
+                                  shadows:[
+                                    Shadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      blurRadius: 7,
+                                      offset: Offset(0, 3),
+                                    )
+                                  ],
+                                  fontFamily:'Quantico',fontWeight:FontWeight.bold,color: Color(0xff5C5C5C),fontSize: 13.0)),
+                            ],
+                          ),
+                        )
+                      ]
+                  )
+              ),
+              Container(
+                  width: 400.0,
+                  height: 149.0,
+                  decoration: BoxDecoration(
+                    color: Color(0xffECECEC),
+                  ),
+                  child: Row(
+                      children:<Widget>[
+                        Container(
+                            width:135,
+                            child: Image.asset('image/채린 미모티콘.png')
+                        ),
+                        Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text('유 채 린 Yu Chae Rin' ,style: TextStyle(
+                                  shadows:[
+                                    Shadow(
+                                      color: Colors.grey.withOpacity(1),
+                                      blurRadius: 7,
+                                      offset: Offset(0, 3),
+                                    )
+                                  ],
+                                  fontFamily:'Quantico',fontWeight:FontWeight.bold,fontSize: 23.0)),
+                              Text('\n컴퓨터공학부 20191191',style: TextStyle(
+                                  shadows:[
+                                    Shadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      blurRadius: 7,
+                                      offset: Offset(0, 3),
+                                    )
+                                  ],
+                                  fontFamily:'Quantico',fontWeight:FontWeight.bold,color: Color(0xff5C5C5C),fontSize: 13.0)),
+                              Text('Email : 20191191@vision.hoseo.edu' ,style: TextStyle(
+                                  shadows:[
+                                    Shadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      blurRadius: 7,
+                                      offset: Offset(0, 3),
+                                    )
+                                  ],
+                                  fontFamily:'Quantico',fontWeight:FontWeight.bold,color: Color(0xff5C5C5C),fontSize: 13.0)),
+                            ],
+                          ),
+                        )
+                      ]
+                  )
+              ),
+              Container(
+                  width: 400.0,
+                  height: 149.0,
+                  decoration: BoxDecoration(
+                    color: Color(0xffECECEC),
+                  ),
+                  child: Row(
+                      children:<Widget>[
+                        Container(
+                            width:135,
+                            child: Image.asset('image/민성 미모티콘.png')
+                        ),
+                        Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text('정 민 성 Jung Min Sung' ,style: TextStyle(
+                                  shadows:[
+                                    Shadow(
+                                      color: Colors.grey.withOpacity(1),
+                                      blurRadius: 7,
+                                      offset: Offset(0, 3),
+                                    )
+                                  ],
+                                  fontFamily:'Quantico',fontWeight:FontWeight.bold,fontSize: 22.5)),
+                              Text('\n컴퓨터공학부 20191251',style: TextStyle(
+                                  shadows:[
+                                    Shadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      blurRadius: 7,
+                                      offset: Offset(0, 3),
+                                    )
+                                  ],
+                                  fontFamily:'Quantico',fontWeight:FontWeight.bold,color: Color(0xff5C5C5C),fontSize: 13.0)),
+                              Text('Email : 20191251@vision.hoseo.edu' ,style: TextStyle(
+                                  shadows:[
+                                    Shadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      blurRadius: 7,
+                                      offset: Offset(0, 3),
+                                    )
+                                  ],
+                                  fontFamily:'Quantico',fontWeight:FontWeight.bold,color: Color(0xff5C5C5C),fontSize: 13.0)),
+                            ],
+                          ),
+                        )
+                      ]
+                  )
+              ),
+              Container(
+                  width: 400.0,
+                  height: 149.0,
+                  decoration: BoxDecoration(
+                    color: Color(0xffECECEC),
+                  ),
+                  child: Row(
+                      children:<Widget>[
+                        Container(
+                            width:135,
+                            child: Image.asset('image/유경 미모티콘.png')
+                        ),
+                        Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text('고 유 경 Go Yu Gyeong' ,style: TextStyle(
+                                  shadows:[
+                                    Shadow(
+                                      color: Colors.grey.withOpacity(1),
+                                      blurRadius: 7,
+                                      offset: Offset(0, 3),
+                                    )
+                                  ],
+                                  fontFamily:'Quantico',fontWeight:FontWeight.bold,fontSize: 22.5)),
+                              Text('\n컴퓨터공학부 20191088',style: TextStyle(
+                                  shadows:[
+                                    Shadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      blurRadius: 7,
+                                      offset: Offset(0, 3),
+                                    )
+                                  ],
+                                  fontFamily:'Quantico',fontWeight:FontWeight.bold,color: Color(0xff5C5C5C),fontSize: 13.0)),
+                              Text('Email : 20191088@vision.hoseo.edu' ,style: TextStyle(
+                                  shadows:[
+                                    Shadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      blurRadius: 7,
+                                      offset: Offset(0, 3),
+                                    )
+                                  ],
+                                  fontFamily:'Quantico',fontWeight:FontWeight.bold,color: Color(0xff5C5C5C),fontSize: 13.0)),
+                            ],
+                          ),
+                        )
+                      ]
+                  )
+              ),
+            ],
+          ),
         ),
       ),
     );
