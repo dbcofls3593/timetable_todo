90169f2917fad4a1ef243546ae74c4ec6aed3b8c
Daeun Kim <gammaeom1@naver.com>
2022-06-06 00:17:08 +0900

    Delete : myTable.dart Simplified code

--- timetable_todo/lib/timetableSource/myTable.dart
+++ timetable_todo/lib/timetableSource/myTable.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-Widget myTable(String week, Color col,
-    {int startnum = 0, int endnum = 0, String subject = ""}) {
+Widget myTable(String week) {
   return Expanded(
     child: Table(
-      border: TableBorder( // 월~일 구역 나누는 선
+      border: TableBorder(
           left: BorderSide(
               color: Colors.grey.shade300)
       ),
@@ -21,39 +20,22 @@ Widget myTable(String week, Color col,
             ]),
         for (int i = 0; i < 10; i++)
           TableRow(children: [
-            i >= startnum && i < endnum?
             GestureDetector(
               onTap: (){
                 print("timetable click");
               },
-              // behavior: HitTestBehavior.opaque,
               child: Container( // 색 박스 칸 나눔
-                child: i==startnum ? Text(subject.toString(), style: TextStyle(fontSize: 11)):null,
                 decoration: BoxDecoration(
-                  color:col,
                   border: Border(
                     top: BorderSide(
                         width: 1,
-                        color:Colors.red),
+                        color: Colors.grey.shade300),
                     bottom: BorderSide(width: 0, color: Colors.transparent),
                   ),
                 ),
                 height: 50.0,
               ),
-            ):
-            Container(
-              decoration: BoxDecoration(
-                color:Colors.transparent,
-                border: Border(
-                  top: BorderSide(
-                      width: 1,
-                      color:  Colors.grey.shade300),
-                  bottom: BorderSide(width: 0, color: Colors.transparent),
-                ),
-              ),
-              height: 50.0,
-            )
-            ,
+            ),
           ]
           ),
       ],
