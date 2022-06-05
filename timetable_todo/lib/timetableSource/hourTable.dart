cbc6f7317cc291df88787fbc6f1d1490aae9bedc
Daeun Kim <gammaeom1@naver.com>
2022-06-06 00:16:37 +0900

    Delete : hourTable.dart simplified code

--- timetable_todo/lib/timetableSource/hourTable.dart
+++ timetable_todo/lib/timetableSource/hourTable.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-Widget hourTable(String week, Color col, {int startnum = 0, int endnum = 0, String subject = ""}) {
+Widget hourTable() {
   return Expanded(
     child: Table(
       children: [
@@ -9,22 +9,17 @@ Widget hourTable(String week, Color col, {int startnum = 0, int endnum = 0, Stri
             children: [
               Container(
                   height: 30.0,
-                  child: Center(
-                      child: Text(
-                        week,
-                      ))),
+                  ),
             ]),
         for (int i = 1; i < 11; i++)
           TableRow(children: [
             Container( // 색 박스 칸 나눔
               child: Text("$i교시"),
               decoration: BoxDecoration(
-                color:col,
                 border: Border(
                   top: BorderSide(
                       width: 1,
                       color:Colors.grey.shade300),
-                  bottom: BorderSide(width: 10, color: Colors.transparent),
                 ),
               ),
               height: 50.0,
