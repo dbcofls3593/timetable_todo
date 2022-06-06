e9511edb7bb9e3a960b28ab4532a2fd3c410d9af
Daeun Kim <gammaeom1@naver.com>
2022-06-07 00:58:25 +0900

    Update : lectureCreateDialog 소스코드 설명

--- timetable_todo/lib/Dialog/lectureCreateDialog.dart
+++ timetable_todo/lib/Dialog/lectureCreateDialog.dart
@@ -24,15 +24,10 @@ class CreateDialog extends StatefulWidget {
   _MyAlertDialogState createState() => _MyAlertDialogState();
 }
 
-
 class _MyAlertDialogState extends State<CreateDialog> {
 
   final List<int> _startList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-
   final List<int> _endList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-
   final List<String> _dateList = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일'];
 
    int id=1;
@@ -43,20 +38,20 @@ class _MyAlertDialogState extends State<CreateDialog> {
               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(17.0)),
               title: Text('수업 추가', style: TextStyle(fontWeight : FontWeight.bold)),
               content:
-              Container(
+              Container( //수업명, 교수명, 시간, 장소, 색상을 설정하는 Container
                 width: 500,
                 padding: EdgeInsets.only(left: 20, right: 20),
-                child: SingleChildScrollView(
+                child: SingleChildScrollView( //행과 열에 스크롤 기능
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
                       children: [
-                        TextField(
+                        TextField( //수업명 입력
                           decoration: InputDecoration(labelText: '수업명'),
                           style: TextStyle(fontWeight : FontWeight.bold),
                           keyboardType: TextInputType.text,
                           controller: widget.subjectsController,
                         ),
-                        TextField(
+                        TextField( //교수명 입력
                             decoration: InputDecoration(labelText: '교수명'),
                             style: TextStyle(fontWeight : FontWeight.bold),
                             keyboardType: TextInputType.text,
@@ -91,7 +86,7 @@ class _MyAlertDialogState extends State<CreateDialog> {
                                   )
                               ),
                             ),
-                            Container(
+                            Container( //시작 교시 설정을 담고 있는 Container
                                 child: Padding(
                                   padding: const EdgeInsets.only(right: 2.0),
                                   child: DropdownButton<int>(
@@ -111,7 +106,7 @@ class _MyAlertDialogState extends State<CreateDialog> {
                                 )
                             ),
                             Text(" 부터 "),
-                            Container(
+                            Container( //끝 교시 설정을 담고 있는 Container
                                 child: Padding(
                                   padding: const EdgeInsets.only(left: 2.0),
                                   child: DropdownButton<int>(
@@ -133,7 +128,7 @@ class _MyAlertDialogState extends State<CreateDialog> {
 
                           ],
                         ),
-                        TextField(
+                        TextField( //장소 입력
                             decoration: InputDecoration(labelText: '장소'),
                             style: TextStyle(fontWeight : FontWeight.bold),
                             keyboardType: TextInputType.text,
@@ -148,9 +143,9 @@ class _MyAlertDialogState extends State<CreateDialog> {
                           ),
                         ),
                         Center(
-                          child: Column(
+                          child: Column( //설정 가능한 색상을 보여주는 Column
                             children: [
-                            Row(
+                            Row( //색상 설정
                             mainAxisAlignment: MainAxisAlignment.start,
                             children: [
                               Radio(
@@ -168,8 +163,7 @@ class _MyAlertDialogState extends State<CreateDialog> {
                                   groupValue:id,
                                   fillColor : MaterialStateColor.resolveWith((states) => Color(0xffD5EFD0)),
                                   onChanged: (val){
-                                    setState((){
-  //_colorSelect = 2;
+                                    setState((){//_colorSelect = 2;
                                       widget.newColorname= Color(0xffD5EFD0);
                                       id = 2;
                                     });
@@ -273,7 +267,7 @@ class _MyAlertDialogState extends State<CreateDialog> {
               actions: <Widget>[
                 TextButton(
                     onPressed: () {
-                      setState(() {
+                      setState(() { //버튼을 눌렀을 때 값이 객체 안으로 들어가며 상태가 설정됨.
                         widget.lectures.add(Detail(widget.newColorname, widget.subjectsController.text, widget.professorController.text, widget.selectedDateValue, widget.selectedValue1, widget.selectedValue2, widget.placeController.text));
                         print(widget.lectures);
                         widget.subjectsController.clear();
@@ -281,7 +275,7 @@ class _MyAlertDialogState extends State<CreateDialog> {
                         widget.placeController.clear();
 
                       });
-                      Navigator.pop(context);
+                      Navigator.pop(context); //이전 화면을 보여줌
                       Navigator.pushReplacement(
                         context,
                         MaterialPageRoute(builder: (context) => const MyHomePage(title: "TDD")),
